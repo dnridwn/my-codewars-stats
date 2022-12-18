a2127a6a1411837138dfc2c34b2fe49553e7bce9
dnridwn <denridwn123@gmail.com>
2022-12-18 22:08:52 +0700

    enhance

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,11 +8,10 @@ createApp({
             clanName: '',
             skills: '',
             totalCompleted: 0,
+            rank: '',
             honor: '',
             leaderboardPosition: '',
             languageRanks: [],
-
-            currentCompletedKataPage: 0,
             completedKatas: []
         }
     },
@@ -24,21 +23,30 @@ createApp({
 
                     this.name = responseData.name
                     this.clanName = responseData.clan
-                    this.skills = responseData.skills.map(skill => skill[0].toUpperCase() + skill.slice(1, skill.length)).join(', ')
+                    this.skills = responseData.skills.map(skill => {
+                        if (skill.toLowerCase() == 'php') {
+                            return skill.toUpperCase()
+                        }
+                        return skill[0].toUpperCase() + skill.slice(1, skill.length)
+                    }).join(', ')
                     this.totalCompleted = responseData.codeChallenges.totalCompleted
                     this.honor = responseData.honor.toLocaleString('en-US')
                     this.leaderboardPosition = responseData.leaderboardPosition.toLocaleString('en-US')
                     this.languageRanks = toRaw(responseData.ranks.languages)
+                    this.rank = responseData.ranks.overall.name
                 })
                 .catch(error => {
                     console.error(error)
                 });
         },
         getCompletedKata() {
-            axios.get(`https://www.codewars.com/api/v1/users/${this.username}/code-challenges/completed?page=${this.currentCompletedKataPage}`)
+            axios.get(`https://www.codewars.com/api/v1/users/${this.username}/code-challenges/completed?page=0`)
                 .then(response => {
                     const responseData = response.data;
-                    this.completedKatas = responseData.data.splice(0, 5);
+                    this.completedKatas = responseData.data.splice(0, 5).map(completedKata => {
+                        completedKata.completedAtFormatted = new Date(completedKata.completedAt).toLocaleDateString("id-ID")
+                        return completedKata
+                    });
                 })
                 .catch(error => {
                     console.error(error)
